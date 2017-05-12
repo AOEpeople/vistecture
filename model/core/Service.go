a8a2f73ebc8d22af5fd9666e55089a27ebd92ef5
Steffen Ritter <steffen.ritter@aoe.com>
2017-05-12 11:08:02 +0200

    Add Title and Summary to Services, too

--- model/core/Service.go
+++ model/core/Service.go
@@ -2,6 +2,8 @@ package core
 
 type Service struct {
 	Name         string       `json:"name" yaml:"name"`
+	Title        string       `json:"title" yaml:"title"`
+	Summary      string       `json:"summary" yaml:"summary"`
 	Description  string       `json:"description" yaml:"description"`
 	Type         string       `json:"type,omitempty" yaml:"type,omitempty"`
 	IsPublic     bool         `json:"isPublic,omitempty" yaml:"isPublic,omitempty"`
