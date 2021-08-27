d3214e3226f2fc1676e9139b4eea832922b36330
Stanislav Müller <stanislav.mueller@omnevo.net>
2021-08-27 17:34:23 +0200

    Added popper.js and formatted code

--- controller/web/template/js/layoutFunctions.js
+++ controller/web/template/js/layoutFunctions.js
@@ -2,70 +2,68 @@
     File contains methods to manipulate or read the view
  */
 
-import $ from 'jquery'
+import $ from "jquery";
 export default class layout {}
 
+layout.GetGraphConfiguration = function () {
+  return {
+    hierarchicalSortMethod: $("#networkHierarchicalSortMethod").val(),
+    nodeStyle: $("#networkNodeStyle").val(),
+    layout: $("#networkLayout").val(),
+    physics: $("#networkPhysics").prop("checked"),
+    clusterGroups: $("#networkClusterGroups").val(),
+    filterGroups: $("#networkFilterGroups").val(),
+  };
+};
 
-
-layout.GetGraphConfiguration = function() {
-    return {
-        'hierarchicalSortMethod': $("#networkHierarchicalSortMethod").val(),
-        'nodeStyle': $("#networkNodeStyle").val(),
-        'layout': $("#networkLayout").val(),
-        'physics': $("#networkPhysics").prop('checked'),
-        'clusterGroups': $('#networkClusterGroups').val(),
-        'filterGroups': $('#networkFilterGroups').val()
-    }
-}
-
-layout.SetGraphPresets = function(preset) {
-    let  config = {}
-    switch(preset) {
-        case "hieradefault":
-            config = {
-                'hierarchicalSortMethod': "directed",
-                'nodeStyle': "detailed",
-                'layout': "hierarchical",
-                'physics': false
-            }
-            break;
-        case "netdefault":
-            config = {
-                'hierarchicalSortMethod': "",
-                'nodeStyle': "small",
-                'layout': "network",
-                'physics': true
-            }
-            break;
-    }
-
-    $("#networkHierarchicalSortMethod").val(config["hierarchicalSortMethod"])
-    $("#networkLayout").val(config["layout"])
-    $("#networkNodeStyle").val(config["nodeStyle"])
-    $("#networkPhysics").prop('checked', config["physics"])
-
-}
-
-layout.ShowSideContentModal = function(title, commontab,servicetab, deptab) {
-    $( "#sidecontent .close" ).click(function() {
-        layout.hideSideContentModal()
-    })
-    $( "#sidecontent #sidecontent-title" ).html(title)
-    $( "#sidecontent #common" ).html(commontab)
-    $( "#sidecontent #services" ).html(servicetab)
-    $( "#sidecontent #dependencies" ).html(deptab)
-    $( "#sidecontent" ).fadeIn()
-}
-
-
-layout.SetDocumentsMenu = function(documents) {
-  for (var i in documents) {
-      let document = documents[i]
-      let urlname = encodeURI(document)
-      $( "#documentations" ).append( `<a class="dropdown-item" href="/documents/${urlname}" target="blank">${document}</a>` );
+layout.SetGraphPresets = function (preset) {
+  let config = {};
+  switch (preset) {
+    case "hieradefault":
+      config = {
+        hierarchicalSortMethod: "directed",
+        nodeStyle: "detailed",
+        layout: "hierarchical",
+        physics: false,
+      };
+      break;
+    case "netdefault":
+      config = {
+        hierarchicalSortMethod: "",
+        nodeStyle: "small",
+        layout: "network",
+        physics: true,
+      };
+      break;
   }
-}
 
-layout.hideSideContentModal = function() {
-    $( "#sidecontent" ).fadeOut()
-}
+  $("#networkHierarchicalSortMethod").val(config["hierarchicalSortMethod"]);
+  $("#networkLayout").val(config["layout"]);
+  $("#networkNodeStyle").val(config["nodeStyle"]);
+  $("#networkPhysics").prop("checked", config["physics"]);
+};
+
+layout.ShowSideContentModal = function (title, commontab, servicetab, deptab) {
+  $("#sidecontent .close").click(function () {
+    layout.hideSideContentModal();
+  });
+  $("#sidecontent #sidecontent-title").html(title);
+  $("#sidecontent #common").html(commontab);
+  $("#sidecontent #services").html(servicetab);
+  $("#sidecontent #dependencies").html(deptab);
+  $("#sidecontent").fadeIn();
+};
+
+layout.SetDocumentsMenu = function (documents) {
+  for (var i in documents) {
+    let document = documents[i];
+    let urlname = encodeURI(document);
+    $("#documentations").append(
+      `<a class="dropdown-item" href="/documents/${urlname}" target="blank">${document}</a>`
+    );
+  }
+};
+
+layout.hideSideContentModal = function () {
+  $("#sidecontent").fadeOut();
+};
--- controller/web/template/js/vistectureHelper.js
+++ controller/web/template/js/vistectureHelper.js
@@ -1,69 +1,70 @@
-import $ from 'jquery'
+import $ from "jquery";
 
 export default class vistectureHelper {}
 
 //loadVistectureData - loads the vistecture project data
-vistectureHelper.LoadVistectureData = function(selectedSubView, networkFilterGroups, callback) {
+vistectureHelper.LoadVistectureData = function (
+  selectedSubView,
+  networkFilterGroups,
+  callback
+) {
+  if (typeof DATAURL == "undefined") {
+    alert("dataurl missing");
+    return;
+  }
+  if (!DATAURL) {
+    alert("dataurl missing");
+    return;
+  }
+  let ajaxUrl = DATAURL;
+  let params = [];
+  if (selectedSubView != null) {
+    params.push("subview=" + selectedSubView);
+  }
+  if (networkFilterGroups != null) {
+    params.push("filterGroups=" + networkFilterGroups);
+  }
 
-
-    if (typeof DATAURL == 'undefined') {
-        alert('dataurl missing')
-        return
-    }
-    if (!DATAURL) {
-        alert('dataurl missing')
-        return
-    }
-    let ajaxUrl = DATAURL
-    let params = []
-    if (selectedSubView != null) {
-        params.push('subview='+selectedSubView)
-    }
-    if (networkFilterGroups != null) {
-        params.push('filterGroups='+networkFilterGroups)
-    }
-
-    ajaxUrl = ajaxUrl + '?' + params.join('&')
-    $.getJSON( ajaxUrl).done(function(data,statustext,jqXHR) {
-        //check if the status is 200(means everything is okay)
-        if (jqXHR.status == 200)
-        {
-            callback(data)
-        } else {
-            $('#maincontent').html('<div class="mx-auto text-danger m-5" style="max-width: 400px;">Loading error... Server returned invalid statuscode</div>')
-            console.log(data,status)
-        }
-    }).fail(function(data) {
-        $('#maincontent').html('<div class="mx-auto text-danger m-5" style="max-width: 400px;">Loading error... check server connection</div>')
-
+  ajaxUrl = ajaxUrl + "?" + params.join("&");
+  $.getJSON(ajaxUrl)
+    .done(function (data, statustext, jqXHR) {
+      //check if the status is 200(means everything is okay)
+      if (jqXHR.status == 200) {
+        callback(data);
+      } else {
+        $("#maincontent").html(
+          '<div class="mx-auto text-danger m-5" style="max-width: 400px;">Loading error... Server returned invalid statuscode</div>'
+        );
+        console.log(data, status);
+      }
+    })
+    .fail(function (data) {
+      $("#maincontent").html(
+        '<div class="mx-auto text-danger m-5" style="max-width: 400px;">Loading error... check server connection</div>'
+      );
     });
-}
+};
 
-
-
-vistectureHelper.FindApp = function(appId, projectData) {
-    for (var i in projectData.applications) {
-        let app = projectData.applications[i]
-        if (app.id == appId) {
-            return app
-        }
+vistectureHelper.FindApp = function (appId, projectData) {
+  for (var i in projectData.applications) {
+    let app = projectData.applications[i];
+    if (app.id == appId) {
+      return app;
     }
-    return false
-}
+  }
+  return false;
+};
 
-
-vistectureHelper.GetIncomingDependencies = function(projectData, appid) {
-    let incomingDep = []
-    for (var i in projectData.applications) {
-        let app = projectData.applications[i]
-        for (var j in app.dependenciesGrouped) {
-            let appDep = app.dependenciesGrouped[j]
-            if (appDep.application.id == appid) {
-                incomingDep.push(appDep)
-            }
-        }
+vistectureHelper.GetIncomingDependencies = function (projectData, appid) {
+  let incomingDep = [];
+  for (var i in projectData.applications) {
+    let app = projectData.applications[i];
+    for (var j in app.dependenciesGrouped) {
+      let appDep = app.dependenciesGrouped[j];
+      if (appDep.application.id == appid) {
+        incomingDep.push(appDep);
+      }
     }
-    return incomingDep
-}
-
-
+  }
+  return incomingDep;
+};
