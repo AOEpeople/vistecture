6e6e79ddeb35277a8aff5671bfe9a1beea70470e
Stanislav Müller <stanislav.mueller@omnevo.net>
2021-08-31 10:20:08 +0200

    remove formatting

--- controller/web/template/js/applicationInit.js
+++ controller/web/template/js/applicationInit.js
@@ -1,122 +1,84 @@
-import $ from "jquery";
+import $ from 'jquery';
 window.$ = $;
-import "../node_modules/popper.js/dist/popper.js";
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
-import visRenderer from "./visRenderer";
-import layout from "./layoutFunctions";
+import '../node_modules/popper.js/dist/popper.js'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
+import visRenderer from "./visRenderer"
+import layout from './layoutFunctions'
 import vistectureHelper from "./vistectureHelper";
 
 // Dom Ready: Shorthand for $( document ).ready()
-$(function () {
-  layout.SetGraphPresets($("#select-graphpreset").val());
-  applicationInit.DrawConfiguredGraph();
+$(function() {
 
-  $("#select-graphpreset").change(function () {
-    layout.SetGraphPresets($("#select-graphpreset").val());
-    applicationInit.DrawConfiguredGraph();
-  });
-  $("#select-project").change(applicationInit.DrawConfiguredGraph);
-  $("#updateGraphConfiguration").click(applicationInit.DrawConfiguredGraph);
-  $("#networkConfigureForm").change(applicationInit.DrawConfiguredGraph);
+    layout.SetGraphPresets($( "#select-graphpreset" ).val())
+    applicationInit.DrawConfiguredGraph()
+
+    $( "#select-graphpreset" ).change(function() {
+        layout.SetGraphPresets($( "#select-graphpreset" ).val())
+        applicationInit.DrawConfiguredGraph()
+    });
+    $( "#select-project" ).change(applicationInit.DrawConfiguredGraph);
+    $( "#updateGraphConfiguration" ).click(applicationInit.DrawConfiguredGraph);
+    $('#networkConfigureForm').change(applicationInit.DrawConfiguredGraph)
 });
 
-var applicationInit = {};
+var applicationInit = {}
 
-applicationInit.DrawConfiguredGraph = function () {
-  let value = $("#select-graph").val();
-  let config = layout.GetGraphConfiguration();
-  let selectedSubView = $("#select-project").val();
-  let networkFilterGroups = $("#networkFilterGroups").val();
+applicationInit.DrawConfiguredGraph = function() {
+    let value = $("#select-graph").val()
+    let config = layout.GetGraphConfiguration()
+    let selectedSubView = $("#select-project").val()
+    let networkFilterGroups = $("#networkFilterGroups").val()
 
-  vistectureHelper.LoadVistectureData(
-    selectedSubView,
-    networkFilterGroups,
-    function (projectData) {
-      applicationInit.updateProjectDropdown(
-        projectData.availableSubViews,
-        config
-      );
-      applicationInit.updateGroups(
-        projectData.applicationsByGroup,
-        projectData.availableGroups,
-        config
-      );
-      layout.SetDocumentsMenu(projectData.staticDocumentations);
-      visRenderer.RenderNetwork(
-        document.getElementById("maincontent"),
-        projectData,
-        config
-      );
+    vistectureHelper.LoadVistectureData(selectedSubView,networkFilterGroups,function(projectData) {
+        applicationInit.updateProjectDropdown(projectData.availableSubViews, config)
+        applicationInit.updateGroups(projectData.applicationsByGroup, projectData.availableGroups, config)
+        layout.SetDocumentsMenu(projectData.staticDocumentations)
+        visRenderer.RenderNetwork(document.getElementById('maincontent'),projectData, config)
+    })
+}
+
+applicationInit.updateProjectDropdown = function(availableSubViews) {
+    let selected =  $("#select-project").val()
+    $("#select-project").find('option').remove()
+    $("#select-project").append(new Option("Select project",""));
+    for (var i in availableSubViews) {
+        let name = availableSubViews[i]
+        let selected = false
+        if (selected == name) {
+            selected = true
+        }
+        $("#select-project").append(new Option(name,name,selected,selected));
     }
-  );
-};
+}
 
-applicationInit.updateProjectDropdown = function (availableSubViews) {
-  let selected = $("#select-project").val();
-  $("#select-project").find("option").remove();
-  $("#select-project").append(new Option("Select project", ""));
-  for (var i in availableSubViews) {
-    let name = availableSubViews[i];
-    let selected = false;
-    if (selected == name) {
-      selected = true;
+
+applicationInit.updateGroups = function(applicationsByGroup,availableGroups) {
+    let selectedClusterOptions =  $("#networkClusterGroups").val()
+    $("#networkClusterGroups").find('option').remove()
+    applicationInit._addSubGroups("#networkClusterGroups",applicationsByGroup.subGroups,selectedClusterOptions, 0)
+
+    let selectedFilterOptions =  $("#networkFilterGroups").val()
+    $("#networkFilterGroups").find('option').remove()
+    applicationInit._addSubGroups("#networkFilterGroups",availableGroups.subGroups,selectedFilterOptions, 0)
+}
+
+
+
+applicationInit._addSubGroups = function(selector, subGroups, selectedOptions, level) {
+    if (level > 9) {
+        return
     }
-    $("#select-project").append(new Option(name, name, selected, selected));
-  }
-};
+    for (var i in subGroups) {
+        let subGroup = subGroups[i]
+        if (subGroup.groupName == "") {
+            continue
+        }
+        let selected = false
+        if ($.inArray( subGroup.qualifiedGroupName, selectedOptions ) != -1) {
+            selected = true
+        }
 
-applicationInit.updateGroups = function (applicationsByGroup, availableGroups) {
-  let selectedClusterOptions = $("#networkClusterGroups").val();
-  $("#networkClusterGroups").find("option").remove();
-  applicationInit._addSubGroups(
-    "#networkClusterGroups",
-    applicationsByGroup.subGroups,
-    selectedClusterOptions,
-    0
-  );
-
-  let selectedFilterOptions = $("#networkFilterGroups").val();
-  $("#networkFilterGroups").find("option").remove();
-  applicationInit._addSubGroups(
-    "#networkFilterGroups",
-    availableGroups.subGroups,
-    selectedFilterOptions,
-    0
-  );
-};
-
-applicationInit._addSubGroups = function (
-  selector,
-  subGroups,
-  selectedOptions,
-  level
-) {
-  if (level > 9) {
-    return;
-  }
-  for (var i in subGroups) {
-    let subGroup = subGroups[i];
-    if (subGroup.groupName == "") {
-      continue;
+        $(selector).append(new Option("  > ".repeat(level)+subGroup.groupName,subGroup.qualifiedGroupName,selected,selected));
+        applicationInit._addSubGroups(selector,subGroup.subGroups,selectedOptions,level+1)
     }
-    let selected = false;
-    if ($.inArray(subGroup.qualifiedGroupName, selectedOptions) != -1) {
-      selected = true;
-    }
-
-    $(selector).append(
-      new Option(
-        "  > ".repeat(level) + subGroup.groupName,
-        subGroup.qualifiedGroupName,
-        selected,
-        selected
-      )
-    );
-    applicationInit._addSubGroups(
-      selector,
-      subGroup.subGroups,
-      selectedOptions,
-      level + 1
-    );
-  }
-};
+}
